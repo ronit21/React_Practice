83f5ebe65c8a40b716872b3d548570932fe5174f
ronit21 <ronit.gujjar333@gmail.com>
2023-07-14 11:06:39 +0530

    Add CSS style inside React

--- reactpractice/src/components/Heading.js
+++ reactpractice/src/components/Heading.js
@@ -1,6 +1,23 @@
 import React from 'react'
 function Heading (props){
-    return (<h1>Hello {props.username} Heading Component with JSX</h1>)
+    /*
+        3 things to note while using CSS style
+        1. Key used as Camelcase. [Example] In CSS :- border-radius => In React :- borderRadius (without any hyphen)
+        2. Values should be inside quotation ('') 
+        3. Colon (:) used instead of equal(=)
+    */
+    const headingStyle = {
+        color : 'white',
+        backgroundColor : 'blue',
+        borderRadius : '2px',
+        border : '1px solid black',
+        width : '400px',
+        margin : '10px',
+        textAlign : 'center'
+    }
+    return (
+    <h1 style={headingStyle}>Hello {props.username} Heading Component with JSX</h1>
+    )
     // return /*#__PURE__*/React.createElement("h1",null, "Heading Component with plain Javascript")
 }
 export default Heading;
